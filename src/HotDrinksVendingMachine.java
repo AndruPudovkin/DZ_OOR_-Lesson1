488700386afa07fbd0ae019e821690a81c9c0cd8
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:42:12 +0500

    Update HotDrinksVendingMachine.java

--- src/HotDrinksVendingMachine.java
+++ src/HotDrinksVendingMachine.java
@@ -1,2 +1,28 @@
 package PACKAGE_NAME;public class HotDrinksVendingMachine {
+  
+    List<HotTea> hotTeas;
+    public  HotDrinksVendingMachine(List<HotTea> HotDrink){
+        this.hotTeas = HotDrink;
+    }
+
+    public List<HotTea> getHotTeas() {
+        return hotTeas;
+    }
+
+    public void setHotTeas(List<HotTea> hotTeas) {
+        this.hotTeas = hotTeas;
+    }
+
+    @Override
+    public HotDrink getProduct(int name, int volume, int temperature) {
+        for (HotTea item: hotTeas){
+            if(item.getName()==name && item.getVolume()==volume && item.getTemperature()==temperature){
+                return item;
+            }
+        }
+        return null;
+    }
+    public void addHotTea (HotTea hotTea){
+        this.hotTeas.add(hotTea);
+    }
 }
