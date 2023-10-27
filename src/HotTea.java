58fb5fa9a9e0f14ddb2cbe0d518c52d4d4de888e
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:42:35 +0500

    Update HotTea.java

--- src/HotTea.java
+++ src/HotTea.java
@@ -1,2 +1,29 @@
 package PACKAGE_NAME;public class HotTea {
+
+    int temperature;
+    public HotTea(int name, int volume) {
+        super(name, volume);
+    }
+
+    public HotTea(int name, int volume, int temperature) {
+        super(name, volume);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "HotTea{" +
+                "name = " + super.getName() +
+                ";volume = " + super.getVolume() +
+                ";temperature = " + temperature +
+                '}';
+    }
 }
