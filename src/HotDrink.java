1dd584a57aea05ab213f5e666d4fa154f1a61e36
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:41:47 +0500

    Update HotDrink.java

--- src/HotDrink.java
+++ src/HotDrink.java
@@ -1,2 +1,26 @@
 package PACKAGE_NAME;public class HotDrink {
+  
+    private int name;
+    private  int volume;
+
+    public HotDrink(int name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 }
